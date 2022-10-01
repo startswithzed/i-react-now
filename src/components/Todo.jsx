66ca6161c27b85a0a2a01b5444363cda537a91f7
Zaid <60604829+startswithzed@users.noreply.github.com>
2022-10-01 07:55:10 +0000

    feat(todo): add on click handler

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,16 @@
 import '../index.css'
 
 function Todo(props) {
+
+  function deleteHandler() {
+    console.log(props.title)
+  }
+
   return (
     <div className='card'>
       <h2>{props.title}</h2>
       <div className='actions'>
-        <button className='btn'>Delete</button>
+        <button className='btn' onClick={deleteHandler}>Delete</button>
       </div>
     </div>
   )
