8f7b48578d9d6878593963dde5e434443a2228dd
Zaid <60604829+startswithzed@users.noreply.github.com>
2022-10-01 12:02:52 +0000

    feat(routes): add all, new and fav meetups routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import './index.css'
+import { Route, Routes } from 'react-router-dom'
+import AllMeetupsPage from './pages/AllMeetups'
+import NewMeetupPage from './pages/NewMeetup'
+import FavouritesPage from './pages/Favourites'
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Routes>
+        <Route exact path='/' element={ <AllMeetupsPage />} />
+        <Route exact path='/new' element={ <NewMeetupPage />} />
+        <Route exact path='/favs' element={ <FavouritesPage />} />
+      </Routes>
+    </div>
   )
 }
 
