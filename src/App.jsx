aa79b3effeb094988bf3797d1c21c6b6c02f8d40
Zaid <60604829+startswithzed@users.noreply.github.com>
2022-10-01 11:15:28 +0000

    chore: clean up project for new project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import './index.css'
-import Todo from './components/Todo'
 
 function App() {
   return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo title='First todo'/>
-      <Todo title='Second todo'/>
-      <Todo title='Third todo'/>
-    </div>
+    <div></div>
   )
 }
 
--- src/components/Backdrop.jsx
+++
@@ -1,7 +0,0 @@
-import '../index.css'
-
-function Backdrop(props) {
-  return <div className='backdrop' onClick={props.onCancel}/>
-}
-
-export default Backdrop
