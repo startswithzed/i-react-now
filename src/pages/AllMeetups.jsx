f8adb03bb35465aef33bf57743d6d41ebe103883
Zaid <60604829+startswithzed@users.noreply.github.com>
2022-10-02 13:15:48 +0000

    feat(all-meetups): fetch all meetups from db

--- src/pages/AllMeetups.jsx
+++ src/pages/AllMeetups.jsx
@@ -1,30 +1,39 @@
+import { useState, useEffect } from 'react'
+
 import MeetupList from '../components/meetup/MeetupList'
 
-const MEETUPS_DATA = [
-  {
-    id: 'm1',
-    title: 'This is a first meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    desc:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-  {
-    id: 'm2',
-    title: 'This is a second meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    desc:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  }
-]
-
 function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [loadedMeetups, setLoadedMeetups] = useState([])
+  const DB_URL = '<DB_URL>'
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetch(DB_URL).then(response => {
+      return response.json()
+    }).then(data => {
+      const meetups = []
+      for (const key in data) {
+        const meetup = {
+          id: key,
+          ...data[key]
+        }
+        meetups.push(meetup)
+      }
+      setIsLoading(false)
+      setLoadedMeetups(meetups)
+    })
+  }, [])
+
+  if (isLoading) {
+    return <section>
+      <h1>Loading...</h1>
+    </section>
+  }
+
   return <section>
     <h1>All Meetups</h1>
-    <MeetupList meetups={MEETUPS_DATA}/>
+    <MeetupList meetups={loadedMeetups} />
   </section>
 }
 
